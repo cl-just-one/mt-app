1eb3a666bb37e43d2b537e530b000de25d17b7ba
chenglin <837460387@qq.com>
2018-12-27 15:19:41 +0800

    search input hotplace seachList code

--- components/public/header/searchBar.vue
+++ components/public/header/searchBar.vue
@@ -12,24 +12,28 @@
         :span="15"
         class="center">
         <div class="wrapper">
-          <el-input placeholder="搜索商家或地点"/>
-          <button class="el-button el-button-primary">
+          <el-input
+            v-model="search"
+            placeholder="搜索商家或地点"
+            @focus="focus"
+            @blur="blur"/>
+          <button class="el-button el-button--primary">
             <i class="el-icon-search"/>
           </button>
-          <dl class="hotPlace">
+          <dl
+            v-if="isHotPlace"
+            class="hotPlace">
             <dt>热门搜索</dt>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
+            <dd
+              v-for="(item, idx) in hotPlace"
+              :key="idx">{{ item }}</dd>
           </dl>
-          <dl class="searchList">
-            <dd>火锅</dd>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
+          <dl
+            v-if="isSearchList"
+            class="searchList">
+            <dd
+              v-for="(item, idx) in searchList"
+              :key="idx">{{ item }}</dd>
           </dl>
         </div>
         <p class="suggest">
@@ -91,6 +95,30 @@
 
 <script>
 export default {
+  data() {
+    return {
+      search: '',
+      isFocus: false,
+      hotPlace: ['火锅','火锅','火锅','火锅'],
+      searchList: ['故宫','故宫','故宫','故宫']
+    }
+  },
+  computed: {
+    isHotPlace: function () {
+      return this.isFocus&&!this.search
+    },
+    isSearchList: function () {
+      return this.isFocus&&this.search
+    }
+  },
+  methods: {
+    focus: function () {
+      this.isFocus = true
+    },
+    blur: function () {
+      this.isFocus = false
+    }
+  }
 }
 </script>
 
